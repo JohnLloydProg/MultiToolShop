90c6f1d42e1bafc2fc1f0ad43a605504ab96bffa
JohnLloydProg <johnlloydunida0@gmail.com>
2025-10-23 16:24:33 +0800

    Created the products page

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
-import { Cart } from './cart/cart';
 import { Orders } from './orders/orders';
 import { About } from './about/about';
 import { Products } from './products/products';
 
 export const routes: Routes = [
-    {path:"", component:Home}, {path:"cart", component:Cart},
+    {path:"", component:Home},
     {path:"orders", component:Orders}, {path:"about", component:About},
     {path:"products", component:Products}
 ];
--- Frontend/src/app/products/products.ts
+++ Frontend/src/app/products/products.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ProductCard } from '../components/product-card/product-card';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './products.html',
   styleUrl: './products.less'
 })
 export class Products {
+  showAvailability:boolean = false;
+  showTypes:boolean = false;
 
+  clickedAvailability() {
+    this.showAvailability = !this.showAvailability;
+  }
+
+  clickedTypes() {
+    this.showTypes = !this.showTypes;
+  }
 }
